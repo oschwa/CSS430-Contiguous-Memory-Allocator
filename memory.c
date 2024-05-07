8dd04e3ae288602adfffcc187a985800cc3bc525
oschwa <oschwa@uw.edu>
2024-05-06 18:04:57 -0700

    Added compact function

--- memory.c
+++ memory.c
@@ -10,6 +10,20 @@ void showMem() {
     printf("\n\n");
 }
 
+void compact() {
+    for (int i = 1; i < MEM_SIZE; i++) {
+        if (mem[i] != '\0' && mem[i - 1] == '\0') {
+            for (int j = i - 1; j >= 0; j--) {
+                if (mem[j] != '\0') {
+                    mem[j + 1] = mem[i];
+                    mem[i] = '\0';
+                    break;
+                }
+            }
+        }
+    }
+}
+
 bool firstFit(char process, int size) {
     //  find the first pocket that fits.
     int a = -1; int b = -1;
@@ -44,6 +58,9 @@ bool worstFit(char process, int size) {
         } else if (mem[i] == '\0') {   
             ++b;
         } else {
+            //  if the current window is bigger
+            //  than the known biggest, then 
+            //  update. 
             if (b - a > wB - wA) {
                 wA = a;
                 wB = b;
@@ -59,6 +76,39 @@ bool worstFit(char process, int size) {
     //  allocate to available memory.
     wB = wA + (size - 1);
     for (int i = wA; i <= wB; i++) {
+        mem[i] = process;
+    }
+    return true;
+}
+
+bool bestFit(char process, int size) {
+    int bA = -1; int bB = -1;
+    int a = -1; int b = -1;
+    for (int i = 0; i < MEM_SIZE; i++) {
+        if (mem[i] == '\0' && a == -1) { 
+            a = i; b = i;
+        } else if (mem[i] == '\0') {   
+            ++b;
+        } else {
+            //  if the current window is smaller than the 
+            //  current minimum window, yet big enough to
+            //  fit the process, then assign new 
+            //  values.
+            if (b - a < bB - bA && (b - a) >= size) {
+                bA = a;
+                bB = b;
+            } else { a = -1; b = -1; }
+        }
+    }
+    if (bA == -1 && bB == -1) {
+        bA = a;
+        bB = b;
+    }
+    //  determine if there is space.
+    if (bB - bA < (size - 1)) { return false; }
+    //  allocate to available memory.
+    bB = bA + (size - 1);
+    for (int i = bA; i <= bB; i++) {
         mem[i] = process;
     }
     return true;
@@ -86,6 +136,10 @@ void allocate(char process, int size, char algorithm) {
         if (!worstFit(process, size)) {
             printf("    ERROR: Memory does not have sufficient space\n     - Please deallocate a process to allocate %c.\n", process);
         }
+    } else if (algorithm == 'B') {
+        if (!bestFit(process, size)) {
+            printf("    ERROR: Memory does not have sufficient space\n     - Please deallocate a process to allocate %c.\n", process);
+        }
     }
 }
 
@@ -107,6 +161,8 @@ int main() {
             int size;
             scanf(" %c %d %c", &process, &size, &algorithm);
             allocate(process, size, algorithm);
+        } else if (cmd == 'C') {
+            compact();
         } else if (cmd == 'F') {
             char process;
             scanf(" %c", &process);
--- memory.h
+++ memory.h
@@ -10,8 +10,10 @@ char mem[MEM_SIZE];
 
 bool read(const char * fileName);
 void showMem();
+void compact();
 bool firstFit(char process, int size);
 bool worstFit(char process, int size);
+bool bestFit(char proces, int size);
 void deallocate(char process);
 void allocate(char process, int size, char algorithm);
 int main();
